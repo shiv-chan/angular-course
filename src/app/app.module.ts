389f58845ff1f911d13493833c3bb70fd2300726
Kaho Shibuya <kahoshibuya@Kahos-MacBook-Air.local>
2023-11-22 16:11:42 -0800

    add course-card component in app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { CourseCardComponent } from './course-card/course-card.component';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CourseCardComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
